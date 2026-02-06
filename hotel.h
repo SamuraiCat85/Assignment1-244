2b02dc97a45ce6f29edec29a11e18ef5d415eea0
prisca <159042680+prisccaa@users.noreply.github.com>
2026-02-06 02:57:19 -0500

    Reservation manager incomplete. Missing exception handling and dynamically allocated Guest_Res_Request object creation.
    Added const qualifiers to getter methods and updated constructor signatures for better clarity and const-correctness.

--- hotel.h
+++ hotel.h
@@ -1,6 +1,8 @@
 #ifndef HOTEL_H_
 #define HOTEL_H_
 
+#include <array>
+
 class Date{
     private:
         int month, day, year;
@@ -39,8 +41,8 @@ class Information{
         void print_info() const;
         string InfoToString() const;
         
-        char* get_fname();
-        char* get_lname();
+        char* get_fname() const;
+        char* get_lname() const;
         Date get_DOB() const;
 
         void set_fname(char*);
@@ -60,30 +62,65 @@ class Guests{
         int roomNumber;
 
     public:
-        Guests();
-        Guests(int,int,int,int,int,int,int);
-        string toString();
-        void AddGuest(const Information&);
+        Guests(); //Default constructor
+        Guests(int,int,int,int,int,int,int); // Parametrized constructor
+        string GuestsToString() const; // Convert guest info to string and return it
+        void AddGuest(const Information&); // Add guest to room
+        
+        Date get_checkIn() const;
+        Date get_checkOut() const;
+        Information get_GuestInfo(int);
+        int get_numberGuests() const;
+        int get_roomNumber() const;
+
+        void set_checkIn(const Date&);
+        void set_checkOut(const Date&);
+        void set_numGuests(int&);
+        void set_roomNumber(int&);
 
 };
 
 class Guests_Res_Request{
     private:
         Guests guest;
-        static int resNumGen; //reservation number generator/counter
-        int resNum;
-    //reservation number
+        static int resNumGen; // reservation number generator/counter
+        int resID; // Reservation number
+        int numNights;
 
     public:
         Guests_Res_Request();
-        Guests_Res_Request() : resNum(){};
+        Guests_Res_Request(Guests, int, int);
+
+        Guests get_Guests() const;
+        int get_resID() const;
+        int get_numNights() const;
+
+        void set_FullGuestInfo(Guests);
+        void set_resID(int);
+        void set_numNights(int);
 
 };
 
 class Reservation_Manager{
+    private:
+        const int max_no_of_nights = 7;
+        const int no_of_rooms = 20;
+        Guests_Res_Request* reservations; 
+        array <array<int, 7>, 20> arr; // I get an error when I put max_no_of_nights and no_of_rooms...
+        // all empty spots of array must be filled with zeroes
+    public:
+        Reservation_Manager();
+        Reservation_Manager(); // Parametrized constructor...        
+        
+        void printReservations() const;
+        int processReservationRequest(Guests_Res_Request&);
+        Guests_Res_Request get_DetailsOfReservation(int) const;
+        void cancelReservation(int);
 
+        // Getters
+        // Setters
 
 
 };
 
-#endif 
+#endif
